860248ae53fd96694fc47c90747c4ec5b8c52b1d
Bluedrack28 <Bluedrack28@gmail.com>
2016-09-03 15:39:15 +0200

    0.2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(port);
-		EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(port);
-		
+		EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.C);
+		EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3ColorSensor colorLigne = new EV3ColorSensor(SensorPort.S1);
+		EV3ColorSensor colorBlock = new EV3ColorSensor(SensorPort.S3);
+		EV3MediumRegulatedMotor motorBras = new EV3MediumRegulatedMotor(MotorPort.A);
 		
 		Orelian or = new Orelian();
 		Loic l = new Loic();
