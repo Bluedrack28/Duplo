c984af2fa856a573dc0b3383975979c4d0d34976
Bluedrack28 <Bluedrack28@gmail.com>
2016-09-03 15:30:58 +0200

    0.1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(port);
+		EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(port);
+		
+		
+		Orelian or = new Orelian();
+		Loic l = new Loic();
+		while(or.restABloc){
+			
+			l.goToTheNextBloc();
+			if(l.getColor()){
+				or.moveRight();
+			}else{
+				or.moveLeft();
+			}
+			
+		}
+		
+		
 	}
 
 }
