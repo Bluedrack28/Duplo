9bc69d1e583060e05003c10f7c003a937e710eae
loic wermeille <loic.wermeille@gmail.com>
2016-09-10 16:40:48 +0200

    modification mineure de la classe main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import lejos.hardware.motor.Motor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorMode;
 
 public class Main {
 
@@ -12,15 +13,15 @@ public class Main {
 		
 		EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.C);
 		EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.B);
-		EV3ColorSensor colorLigne = new EV3ColorSensor(SensorPort.S1);
-		EV3ColorSensor colorBlock = new EV3ColorSensor(SensorPort.S3);
+		SensorMode colorLigne = new EV3ColorSensor(SensorPort.S1);
+		SensorMode colorBlock = new EV3ColorSensor(SensorPort.S3).getColorIDMode();
 		EV3MediumRegulatedMotor motorBras = new EV3MediumRegulatedMotor(MotorPort.A);
 		
 		Orelian or = new Orelian();
 		Loic l = new Loic();
 		while(or.restABloc){
 			
-			l.goToTheNextBloc(colorBlock);
+			l.goToTheNextBloc(colorBlock, motorR, motorL);
 			if(l.getColor()){
 				or.moveRight();
 			}else{
